564c6623559381500b589be4464725b200f7c3b9
Axel Mejbert <monsdj.be@gmail.com>
2024-11-30 17:24:10 +0100

    "fix: Update Heroku deployment configuration"

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,14 +1,14 @@
 require('dotenv').config();
 
-module.exports = {
+const config = {
   development: {
     client: 'postgresql',
-    connection: {
+    connection: process.env.DATABASE_URL || {
       host: process.env.DB_HOST || 'localhost',
       port: process.env.DB_PORT || 5432,
       database: process.env.DB_NAME || 'monscolis',
       user: process.env.DB_USER || 'postgres',
-      password: process.env.DB_PASSWORD || 'postgres',
+      password: process.env.DB_PASSWORD,
     },
     migrations: {
       directory: './database/migrations',
@@ -20,12 +20,12 @@ module.exports = {
 
   test: {
     client: 'postgresql',
-    connection: {
+    connection: process.env.DATABASE_URL || {
       host: process.env.DB_HOST || 'localhost',
       port: process.env.DB_PORT || 5432,
       database: process.env.DB_NAME || 'monscolis_test',
       user: process.env.DB_USER || 'postgres',
-      password: process.env.DB_PASSWORD || 'postgres',
+      password: process.env.DB_PASSWORD,
     },
     migrations: {
       directory: './database/migrations',
@@ -35,16 +35,25 @@ module.exports = {
     },
   },
 
+  staging: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './database/migrations',
+    },
+    seeds: {
+      directory: './database/seeds',
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    ssl: { rejectUnauthorized: false },
+  },
+
   production: {
     client: 'postgresql',
-    connection: {
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      database: process.env.DB_NAME,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      ssl: { rejectUnauthorized: false },
-    },
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: './database/migrations',
     },
@@ -52,5 +61,8 @@ module.exports = {
       min: 2,
       max: 10,
     },
+    ssl: { rejectUnauthorized: false },
   },
 };
+
+module.exports = config;
